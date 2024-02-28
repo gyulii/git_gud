b37c45992c1bb67bfb0baba935526eb1996c3b2c
Faragó Gyula <faragogyulii@gmail.com>
2024-02-28 13:48:42 +0100

    New feature

---
+++ main2.py
@@ -0,0 +1,6 @@
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    mainWindow = MainWindow()
+    mainWindow.show()
+    sys.exit(app.exec())
--- not_main.py
+++ not_main.py
@@ -1,1 +1,1 @@
-print("hello")
+print("not_hello")
